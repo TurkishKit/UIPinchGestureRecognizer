3d881dfd6e5669b1e3829ae36a0562164a1be315
Ufuk Köşker <ufukkosker@ufuk-imac.local>
2019-03-01 12:42:49 +0300

    Initial Commit

---
+++ UIPinchGestureRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPinchGestureRecognizer
+//
+//  Created by Ufuk Köşker on 1.03.2019.
+//  Copyright © 2019 Ufuk Köşker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
